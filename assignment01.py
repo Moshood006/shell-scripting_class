9a02dad6f970b1f6aa2833c7197cde0c57c146fc
Moshood006 <ibnmasuud@gmail.com>
2023-08-28 15:49:47 +0100

    assignment on linux commands

---
+++ assignment01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3 
+
+Print("the answer to 10 lines of a file top is head -10 assignment01.py") 
+Print("the answer to 10 lines of a file bottom is tail -10 assignment02.py")  
+Print("the answer is assignment01.py | grep title *")
+Print("the answer is touch followed by the file name") 
+Print("the answer is mv assignment01.py assignment_01.py") 
+Print("the answer is cp assignment_01.py mtatechclass.py") 
+Print("the answer is cat assignment01.py")
+
+
+
+
+ 
